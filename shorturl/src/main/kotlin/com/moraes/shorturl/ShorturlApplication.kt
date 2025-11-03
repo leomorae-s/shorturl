6ccaa75f9d1797a83dd1d1c1a2c6db8272016b46
leonardo <dev.leonardomoraes@gmail.com>
2025-11-03 19:36:48 -0300

    removed unused imports

--- shorturl/src/main/kotlin/com/moraes/shorturl/ShorturlApplication.kt
+++ shorturl/src/main/kotlin/com/moraes/shorturl/ShorturlApplication.kt
@@ -2,9 +2,7 @@ package com.moraes.shorturl
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
+
 
 @SpringBootApplication
 class ShorturlApplication
